88142ba8799f589b966f4ee5be19d28382e3415b
ServerToMbak <server78135@gmail.com>
2023-07-24 23:03:28 +0300

    Add project files.

---
+++ FirstBlazorProjetWith.netDocumentation/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FirstBlazorProjetWith.netDocumentation.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstBlazorProjetWith.netDocumentation.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> Categories { get; set; }     
+    }
+}
---
+++ FirstBlazorProjetWith.netDocumentation/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlazorProjetWith.netDocumentation.Model
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
